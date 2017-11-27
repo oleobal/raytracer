74fdc8ef3367a654ea32148688f7ef9dc78c6a6c
Vincent Fabioux <vilcegt@gmail.com>
2017-11-27 19:40:44 +0100

    Added run script and readme.

---
+++ raytracer-binary/runme.bat
@@ -0,0 +1,3 @@
+raytracer.exe scene01-normal.yaml
+raytracer.exe scene01-phong.yaml
+raytracer.exe scene01-zbuffer.yaml
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Students:
+	Vincent Fabioux
+	Olivier Léobal
+
+Week: 2
+
+Nothing notable in this version
